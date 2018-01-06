cff17db648df2b21ddd1d09eead52685642dba54
jung <changun.jung@gmail.com>
2018-01-06 16:09:04 +0900

    leetcode 70 python3 fibonacci

--- leetcode70.py
+++ leetcode70.py
@@ -1,10 +1,52 @@
+import sys
+
 class Solution(object):
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        print n
 
+        steps = 0
+        if n == 0:
+            return 1
+        if n - 1 >= 0:
+            # print "step1" + str(n)
+            # print "step1x" + str(n) + " : " + str(Solution().climbStairs(n-1))
+            steps += Solution().climbStairs(n - 1)
+        if n -2 >= 0:
+            # print "step2" + str(n)
+            # print "step2x" + str(n) + " : " + str(Solution().climbStairs(n-2))
+            steps += Solution().climbStairs(n - 2)
 
-Solution().climbStairs(30)
+        return steps
+
+    def climbStairs_f(self, n):
+        """
+        :type n: int
+        :rtype: int
+        using fibonacci
+        """
+
+        a = 1
+        b = 0
+        while n:
+            a,b = a + b, a
+            n -= 1
+
+        return a
+
+    def climbStairs_f2(self, n):
+        """
+        :type n: int
+        :rtype: int
+        using fibonacci
+        """
+
+        a = b = 1
+        for _ in range(n):
+            a,b = b, a + b
+
+        return a
+
+print Solution().climbStairs_f(int(sys.argv[1]))
